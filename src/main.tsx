2b2563e280eb441df91cd750f4d7fff9f16ea680
Roman Chaban <roman.chaban1001@gmail.com>
2024-03-09 07:02:30 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import * as ReactDOMClient from 'react-dom/client';
+import { App } from './App';
+import './assets/styles/main.module.scss';
+import { styled, createGlobalStyle } from 'styled-components';
+import { Fragment } from 'react/jsx-runtime';
+import { StrictMode } from 'react';
+
+// eslint-disable-next-line react-refresh/only-export-components
+const RootWrapper = createGlobalStyle`
+* {
+  font-family: "Poppins", sans-serif;
+}
+`;
+
+// eslint-disable-next-line react-refresh/only-export-components
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const root = ReactDOMClient.createRoot(
+  document.getElementById('root') as HTMLDivElement
+);
+
+root.render(
+  <StrictMode>
+    <Fragment>
+      <App />
+      <RootWrapper />
+      <AppContainer />
+    </Fragment>
+  </StrictMode>
+);
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+declare module '*.module.scss';
+declare module '*.png';
+declare module '*.webp';
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
